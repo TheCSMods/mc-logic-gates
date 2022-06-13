0232e27561e26e3fa3ad4923c41b788b8da3ca60
TheCSDev <thecsdev@gmail.com>
2022-06-14 00:38:28 +0200

    Release v1.1 for fabric-1.19

---
+++ logicgates-fabric-1.19/src/main/java/thecsdev/logicgates/LogicGatesConfig.java
@@ -0,0 +1,88 @@
+package thecsdev.logicgates;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * All the properties in the config are READ-ONLY.
+ * Modifying them while the game is running will have no effect.
+ */
+public final class LogicGatesConfig
+{
+	/**
+	 * Holds the properties that will be saved/loaded.
+	 */
+	private static Properties PROPERTIES;
+	
+	public static boolean ENABLE_LOGIC_GATE_WIRE = true;
+	public static boolean ENABLE_LOGIC_GATE_WIRE_TURN = true;
+	
+	public static boolean ENABLE_LOGIC_GATE_AND = true;
+	public static boolean ENABLE_LOGIC_GATE_OR = true;
+	public static boolean ENABLE_LOGIC_GATE_XOR = true;
+	public static boolean ENABLE_LOGIC_GATE_NOT = true;
+	public static boolean ENABLE_LOGIC_GATE_NAND = true;
+	public static boolean ENABLE_LOGIC_GATE_NOR = true;
+	public static boolean ENABLE_LOGIC_GATE_XNOR = true;
+	
+	public static boolean ENABLE_LATCH_SETRESET = true;
+	public static boolean ENABLE_LATCH_DATA = true;
+	
+	/**
+	 * Returns the {@link File} where the
+	 * {@link #PROPERTIES} should be stored.
+	 */
+	public static File getPropertiesFile()
+	{
+		return new File(System.getProperty("user.dir") + "/config/" + LogicGates.ModID + ".properties");
+	}
+	
+	/**
+	 * Loads the {@link #PROPERTIES}.
+	 */
+	public static void loadProperties() throws IOException
+	{
+		//get file
+		File pf = getPropertiesFile();
+		LogicGates.LOGGER.info("Loading config from: " + pf.getAbsolutePath());
+		if(!pf.exists())
+		{
+			LogicGates.LOGGER.info("Could not load " + LogicGates.ModID + " config. File not found.");
+			return;
+		}
+		
+		//load PROPERTIES
+		FileInputStream pfIn = new FileInputStream(pf);
+		PROPERTIES = new Properties();
+		PROPERTIES.load(pfIn);
+		pfIn.close();
+		
+		//paste data onto the variables
+		ENABLE_LOGIC_GATE_WIRE = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_WIRE"));
+		ENABLE_LOGIC_GATE_WIRE_TURN = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_WIRE_TURN"));
+		
+		ENABLE_LOGIC_GATE_AND = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_AND"));
+		ENABLE_LOGIC_GATE_OR = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_OR"));
+		ENABLE_LOGIC_GATE_XOR = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_XOR"));
+		ENABLE_LOGIC_GATE_NOT = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_NOT"));
+		ENABLE_LOGIC_GATE_NAND = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_NAND"));
+		ENABLE_LOGIC_GATE_NAND = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_NAND"));
+		ENABLE_LOGIC_GATE_NOR = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_NOR"));
+		ENABLE_LOGIC_GATE_XNOR = smartBool(PROPERTIES.getProperty("ENABLE_LOGIC_GATE_XNOR"));
+		
+		ENABLE_LATCH_SETRESET = smartBool(PROPERTIES.getProperty("ENABLE_LATCH_SETRESET"));
+		ENABLE_LATCH_DATA = smartBool(PROPERTIES.getProperty("ENABLE_LATCH_DATA"));
+	}
+	
+	/**
+	 * idk why i did this. sounds cool ig.
+	 */
+	private static boolean smartBool(String arg0)
+	{
+		String a = arg0.split(" ")[0].toLowerCase();
+		return (a.startsWith("true") || a.startsWith("ye") ||
+				a.startsWith("ok") || a.startsWith("sur")) && a.length() <= 5;
+	}
+}
--- logicgates-fabric-1.19/src/main/java/thecsdev/logicgates/LogicGatesItems.java
+++ logicgates-fabric-1.19/src/main/java/thecsdev/logicgates/LogicGatesItems.java
@@ -13,41 +13,87 @@ import thecsdev.logicgates.item.LogicGateOrItem;
 import thecsdev.logicgates.item.LogicGateWireItem;
 import thecsdev.logicgates.item.LogicGateWireTurnItem;
 
+import static thecsdev.logicgates.LogicGatesConfig.*;
+
 /**
  * Holds all of the blocks from logic gates.
  */
 public class LogicGatesItems
 {
 	// ==================================================
-	public static final LogicGateWireItem LOGIC_GATE_WIRE = new LogicGateWireItem();
-	public static final LogicGateWireTurnItem LOGIC_GATE_WIRE_TURN = new LogicGateWireTurnItem();
+	public static LogicGateWireItem LOGIC_GATE_WIRE;
+	public static LogicGateWireTurnItem LOGIC_GATE_WIRE_TURN;
 	
-	public static final LogicGateAndItem LOGIC_GATE_AND = new LogicGateAndItem();
-	public static final LogicGateOrItem LOGIC_GATE_OR = new LogicGateOrItem();
-	public static final LogicGateExclusiveOrItem LOGIC_GATE_XOR = new LogicGateExclusiveOrItem();
-	public static final LogicGateNotItem LOGIC_GATE_NOT = new LogicGateNotItem();
-	public static final LogicGateNotAndItem LOGIC_GATE_NAND = new LogicGateNotAndItem();
-	public static final LogicGateNotOrItem LOGIC_GATE_NOR = new LogicGateNotOrItem();
-	public static final LogicGateExclusiveNotOrItem LOGIC_GATE_XNOR = new LogicGateExclusiveNotOrItem();
+	public static LogicGateAndItem LOGIC_GATE_AND;
+	public static LogicGateOrItem LOGIC_GATE_OR;
+	public static LogicGateExclusiveOrItem LOGIC_GATE_XOR;
+	public static LogicGateNotItem LOGIC_GATE_NOT;
+	public static LogicGateNotAndItem LOGIC_GATE_NAND;
+	public static LogicGateNotOrItem LOGIC_GATE_NOR;
+	public static LogicGateExclusiveNotOrItem LOGIC_GATE_XNOR;
 	
-	public static final LatchSetResetItem LATCH_SETRESET = new LatchSetResetItem();
-	public static final LatchDataItem LATCH_DATA = new LatchDataItem();
+	public static LatchSetResetItem LATCH_SETRESET;
+	public static LatchDataItem LATCH_DATA;
 	// ==================================================
 	static void registerAll()
 	{
-		Registry.register(Registry.ITEM, LOGIC_GATE_WIRE.getIdentifier(), LOGIC_GATE_WIRE);
-		Registry.register(Registry.ITEM, LOGIC_GATE_WIRE_TURN.getIdentifier(), LOGIC_GATE_WIRE_TURN);
+		if(ENABLE_LOGIC_GATE_WIRE)
+		{
+			LOGIC_GATE_WIRE = new LogicGateWireItem();
+			Registry.register(Registry.ITEM, LOGIC_GATE_WIRE.getIdentifier(), LOGIC_GATE_WIRE);
+		}
+		if(ENABLE_LOGIC_GATE_WIRE_TURN)
+		{
+			LOGIC_GATE_WIRE_TURN = new LogicGateWireTurnItem();
+			Registry.register(Registry.ITEM, LOGIC_GATE_WIRE_TURN.getIdentifier(), LOGIC_GATE_WIRE_TURN);
+		}
 		
-		Registry.register(Registry.ITEM, LOGIC_GATE_AND.getIdentifier(), LOGIC_GATE_AND);
-		Registry.register(Registry.ITEM, LOGIC_GATE_OR.getIdentifier(), LOGIC_GATE_OR);
-		Registry.register(Registry.ITEM, LOGIC_GATE_XOR.getIdentifier(), LOGIC_GATE_XOR);
-		Registry.register(Registry.ITEM, LOGIC_GATE_NOT.getIdentifier(), LOGIC_GATE_NOT);
-		Registry.register(Registry.ITEM, LOGIC_GATE_NAND.getIdentifier(), LOGIC_GATE_NAND);
-		Registry.register(Registry.ITEM, LOGIC_GATE_NOR.getIdentifier(), LOGIC_GATE_NOR);
-		Registry.register(Registry.ITEM, LOGIC_GATE_XNOR.getIdentifier(), LOGIC_GATE_XNOR);
+		if(ENABLE_LOGIC_GATE_AND)
+		{
+			LOGIC_GATE_AND = new LogicGateAndItem();
+			Registry.register(Registry.ITEM, LOGIC_GATE_AND.getIdentifier(), LOGIC_GATE_AND);
+		}
+		if(ENABLE_LOGIC_GATE_OR)
+		{
+			LOGIC_GATE_OR = new LogicGateOrItem();
+			Registry.register(Registry.ITEM, LOGIC_GATE_OR.getIdentifier(), LOGIC_GATE_OR);
+		}
+		if(ENABLE_LOGIC_GATE_XOR)
+		{
+			LOGIC_GATE_XOR = new LogicGateExclusiveOrItem();
+			Registry.register(Registry.ITEM, LOGIC_GATE_XOR.getIdentifier(), LOGIC_GATE_XOR);
+		}
+		if(ENABLE_LOGIC_GATE_NOT)
+		{
+			LOGIC_GATE_NOT = new LogicGateNotItem();
+			Registry.register(Registry.ITEM, LOGIC_GATE_NOT.getIdentifier(), LOGIC_GATE_NOT);
+		}
+		if(ENABLE_LOGIC_GATE_NAND)
+		{
+			LOGIC_GATE_NAND = new LogicGateNotAndItem();
+			Registry.register(Registry.ITEM, LOGIC_GATE_NAND.getIdentifier(), LOGIC_GATE_NAND);
+		}
+		if(ENABLE_LOGIC_GATE_NOR)
+		{
+			LOGIC_GATE_NOR = new LogicGateNotOrItem();
+			Registry.register(Registry.ITEM, LOGIC_GATE_NOR.getIdentifier(), LOGIC_GATE_NOR);
+		}
+		if(ENABLE_LOGIC_GATE_XNOR)
+		{
+			LOGIC_GATE_XNOR = new LogicGateExclusiveNotOrItem();
+			Registry.register(Registry.ITEM, LOGIC_GATE_XNOR.getIdentifier(), LOGIC_GATE_XNOR);
+		}
 		
-		Registry.register(Registry.ITEM, LATCH_SETRESET.getIdentifier(), LATCH_SETRESET);
-		Registry.register(Registry.ITEM, LATCH_DATA.getIdentifier(), LATCH_DATA);
+		if(ENABLE_LATCH_SETRESET)
+		{
+			LATCH_SETRESET = new LatchSetResetItem();
+			Registry.register(Registry.ITEM, LATCH_SETRESET.getIdentifier(), LATCH_SETRESET);
+		}
+		if(ENABLE_LATCH_DATA)
+		{
+			LATCH_DATA = new LatchDataItem();
+			Registry.register(Registry.ITEM, LATCH_DATA.getIdentifier(), LATCH_DATA);
+		}
 	}
 	// ==================================================
 }
